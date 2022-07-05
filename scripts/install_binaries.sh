d23f57570ab2becd97292dcb7519c0ac7cce79f9
BIBO <49994376+BIBOLV@users.noreply.github.com>
2022-07-05 23:27:01 +0300

    Update install_binaries.sh

--- scripts/install_binaries.sh
+++ scripts/install_binaries.sh
@@ -1,9 +1,10 @@
 # Copyright (C) 2021 Fredrik Öhrström (gpl-3.0-or-later)
 
-rm -f "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd
+rm -f "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd "$ROOT"/usr/sbin/wmbusmeters-admin
 mkdir -p "$ROOT"/usr/bin
 mkdir -p "$ROOT"/usr/sbin
 cp "$SRC" "$ROOT"/usr/bin/wmbusmeters
+cp "$SRC" "$ROOT"/usr/bin/wmbusmeters-admin
 (cd "$ROOT"/usr/sbin; ln -s ../bin/wmbusmeters wmbusmetersd)
 
 echo "binaries: installed $ROOT/usr/bin/wmbusmeters and $ROOT/usr/sbin/wmbusmetersd"
